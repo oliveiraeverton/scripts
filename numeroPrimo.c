7e23e73c2cb193d74a6c2c269c25ddc5abf19f61
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2022-07-11 23:00:44 -0400

    Update numeroPrimo.c

--- numeroPrimo.c
+++ numeroPrimo.c
@@ -20,3 +20,13 @@ int main(){
 
     return 0;
 }
+/*
+Os números primos possuem 2 divisores, 1 e eles próprios. 
+No entanto todos os números são divisiveis por 1.
+Se sabemos que todos números são divisíveis por 1, podemos desconsiderar que
+números primos possuem 2 divisores e somente 1, eles mesmos.
+Então se em algum momento n%i == 0, o número n é um número primo, pois
+estou testando dentro do for se n é divisivel do intervalo [2,n-1], ou seja,
+excluímos a opção da conta n%n==0, pois também sabemos que n%n é divisor dele
+mesmo.
+*/
