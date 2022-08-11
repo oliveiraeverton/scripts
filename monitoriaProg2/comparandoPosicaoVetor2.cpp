d11863410a7c4e357ed0f0e9caeaa1c486d3a432
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2022-08-10 23:29:51 -0400

    Update comparandoPosicaoVetor2.cpp

--- monitoriaProg2/comparandoPosicaoVetor2.cpp
+++ monitoriaProg2/comparandoPosicaoVetor2.cpp
@@ -3,18 +3,16 @@
 
 int main(){
 
-    int n = 8, linear=0;
-    int v[8] = {11,14,16,20,17,31,52,90};
+    int n = 0;
+    int v[100] = {0};
 
-    printf("\n");
-    for (int i = 0; i < n; i++){
-        printf("%d ", v[i]);
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &v[i]);
     }
-    printf("\n");
     
     for(int i = 1; i < n; i++){
     
-        //printf("\ncomparando v[%d] < v[%d] == %d", i-1, i, v[i-1] < v[i]);
         
         if(v[i-1]>=v[i]){
             printf("falso");
